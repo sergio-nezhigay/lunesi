6126692de65f0044d21b8860da6ff08e9d501db6
Serhii Nezhyhai <58699786+sergio-nezhigay@users.noreply.github.com>
2026-03-02 10:25:28 +0200

    pulled live

--- assets/facets.js
+++ assets/facets.js
@@ -206,17 +206,8 @@ facetsToRender.forEach((element) => {
     event.preventDefault();
 
     const currentForm = event.target.closest('form');
-    const vatMult = parseFloat(this.dataset.vatMult || 1);
-
     if (currentForm.id === 'FacetFiltersFormMobile') {
       const searchParams = this.createSearchParams(currentForm);
-      if (vatMult !== 1) {
-        searchParams.forEach((value, key) => {
-          if (key === 'filter.v.price.gte' || key === 'filter.v.price.lte') {
-            searchParams.set(key, Math.round(parseFloat(value) / vatMult));
-          }
-        });
-      }
       this.onSubmitForm(searchParams.toString(), event);
     }
     else {
@@ -234,14 +225,6 @@ facetsToRender.forEach((element) => {
           searchParams = this.mergeSearchParams(form, searchParams);
         }
       });
-
-      if (vatMult !== 1) {
-        searchParams.forEach((value, key) => {
-          if (key === 'filter.v.price.gte' || key === 'filter.v.price.lte') {
-            searchParams.set(key, Math.round(parseFloat(value) / vatMult));
-          }
-        });
-      }
 
       this.onSubmitForm(searchParams.toString(), event);
     }
